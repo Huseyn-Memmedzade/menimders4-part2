aaf9579fa97d29e1707b994d833fbf46b1345140
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-07-17 19:44:54 +0400

    Add files via upload

---
+++ DovrTapsiriqlariTesti.java
@@ -0,0 +1,21 @@
+package yenipabka;
+
+import java.util.Scanner;
+
+public class DovrTapsiriqlariTesti {
+
+    public static void main(String[] args) {
+        // 5 defe Salam Sozunu Capa Ver
+        // Hazirdir✅
+        int i = 0;
+        while (i < 5) {
+            i++;
+            System.out.println("Salam");
+
+        }
+    }
+}
+
+
+
+
